b4d978a16d2302f167b087bfea3d51ce5dc4dfe8
Quinner12 <159549861+Quinner12@users.noreply.github.com>
2024-04-04 13:17:50 +0000

    Area test

--- assignment.py
+++ assignment.py
@@ -2,5 +2,8 @@ import math
 print("Please Input, Diameter")
 Diameter = int(input(""))
 circumference = Diameter * math.pi
+Area = 1/4 * math.pi * Diameter ** 2
 print("The circumference is") 
 print (circumference)
+print("The Area is") 
+print (Area)
