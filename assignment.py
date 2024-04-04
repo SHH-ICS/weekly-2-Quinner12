242d97f123b0cb6ce29ea9f152153e10924e001a
Quinner12 <159549861+Quinner12@users.noreply.github.com>
2024-04-04 14:45:23 +0000

    loop for positive  and area working

--- assignment.py
+++ assignment.py
@@ -1,8 +1,14 @@
 import math
-print("Please Input, Diameter")
-Diameter = int(input(""))
+while True:
+    print("Please Input, Diameter") 
+    Diameter = int(input(""))
+    if Diameter < 0:
+        print("Diameter must be positive")
+        continue
+
+    break
 circumference = Diameter * math.pi
-Area = 1/4 * math.pi * Diameter ** 2
+Area = 1/4* math.pi * Diameter ** 2
 print("The circumference is") 
 print (circumference)
 print("The Area is") 
